523afc691725a9ba030bc85e6cd88e2f41a34574
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-10 17:26:15 +0200

    milestone-1-compete

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log(btnSubmit);
 const priceKm = 0.21;
 const discountUnder18 = 0.8;
 const discountOver65 = 0.6;
-let ticketPrice = document.getElementById('ticket_price')
+let ticketPrice = document.getElementById('ticket_price');
 
 
 btnSubmit.addEventListener('click',
@@ -15,21 +15,21 @@ function()
 
     let age = document.getElementById('age').value;
     console.log(age);
-
+    
     if (age >= 65)
     {
-        ticketPrice.innerHTML = distance * priceKm * discountOver65;
+        ticketPrice.innerHTML = (distance * priceKm * discountOver65).toFixed(2) + '€';
     }
 
     else if (age < 18)
     {
-        ticketPrice.innerHTML = distance * priceKm * discountUnder18;
+        ticketPrice.innerHTML = (distance * priceKm * discountUnder18).toFixed(2) + '€';
     }
 
     else
     {
-        ticketPrice.innerHTML = distance * priceKm;
+        ticketPrice.innerHTML = (distance * priceKm).toFixed(2) + '€';
     }
-
-
-})
+    
+    
+});
