62426aba545c60d8ca64908d83b972ae5db9083b
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-10 21:30:13 +0200

    ticket-name-&-discount-done

--- js/ticket.js
+++ js/ticket.js
@@ -16,19 +16,27 @@ function()
     let age = document.getElementById('age').value;
     console.log(age);
     
+    let name = document.getElementById('name').value;
+    console.log(name);
+    
+    document.getElementById('ticket_name').innerHTML = name
+    
     if (age === 'over_65')
     {
         ticketPrice.innerHTML = (distance * priceKm * discountOver65).toFixed(2) + '€';
+        document.getElementById('ticket_discount').innerHTML = 'Biglietto Over 65'
     }
-
+    
     else if (age === 'minorenne')
     {
         ticketPrice.innerHTML = (distance * priceKm * discountUnder18).toFixed(2) + '€';
+        document.getElementById('ticket_discount').innerHTML = 'Biglietto Under 18'
     }
-
+    
     else
     {
         ticketPrice.innerHTML = (distance * priceKm).toFixed(2) + '€';
+        document.getElementById('ticket_discount').innerHTML = 'Biglietto Standard'
     }
     
     
