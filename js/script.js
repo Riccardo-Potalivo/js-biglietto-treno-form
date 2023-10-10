4f60da42e04cf44d83d0d38602770d410b3004c3
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-10 17:03:02 +0200

    operation-done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 const btnSubmit = document.querySelector('#btn_submit');
 console.log(btnSubmit);
 
+const priceKm = 0.21;
+const discountUnder18 = 0.8;
+const discountOver65 = 0.6;
+let ticketPrice = document.getElementById('ticket_price')
+
+
 btnSubmit.addEventListener('click',
 function()
 {
@@ -9,5 +15,21 @@ function()
 
     let age = document.getElementById('age').value;
     console.log(age);
+
+    if (age >= 65)
+    {
+        ticketPrice.innerHTML = distance * priceKm * discountOver65;
+    }
+
+    else if (age < 18)
+    {
+        ticketPrice.innerHTML = distance * priceKm * discountUnder18;
+    }
+
+    else
+    {
+        ticketPrice.innerHTML = distance * priceKm;
+    }
+
 
 })
