49fd1dcd22b032a577e38149e702e23d5c18518c
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-10 16:34:34 +0200

    operation-set

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btnSubmit = document.querySelector('#btn_submit');
+console.log(btnSubmit);
+
+btnSubmit.addEventListener('click',
+function()
+{
+    let distance = document.getElementById('distance').value;
+    console.log(distance);
+
+    let age = document.getElementById('age').value;
+    console.log(age);
+
+})
